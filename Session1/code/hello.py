07cd65a00fae395116e085c4bb5a4ce14e73bf81
Chris Barker <Chris.Barker@noaa.gov>
2013-10-16 11:37:22 -0700

    most of it in shape for Sessions 2 (need to add dicts!)

--- Session1/code/hello.py
+++ Session1/code/hello.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
+from __future__ import division
 
-print "Hello!"
+print "Hello! again",
+print 'that'
 
+def fun(x, y):
+	"""
+	this is a docstring
+	"""
+	return x / y
 
+def fun2():
+	return 'this'
+
+	
